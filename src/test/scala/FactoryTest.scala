e5b08e1db6cb509766777086cb5b36d7445097df
Leonardo <lperugini2@gmail.com>
2020-09-04 17:38:16 +0200

    Fix a bug in creature factory - sprint backlog updated

--- src/test/scala/FactoryTest.scala
+++ src/test/scala/FactoryTest.scala
@@ -12,15 +12,16 @@ class FactoryTest extends AnyFunSuite{
   val strategy : () => Position = () => Position(Random.nextInt(bounds.bottomRight.x.toInt), Random.nextInt(bounds.bottomRight.x.toInt))
   val foodSet: Set[Food] = Food(100, 10)(strategy)
 
-//  val creatureSet: Set[Creature] = Creature.makeSet(100, 10, 10, 10)(strategy)
+  val creatureSet: Set[Creature] = Creature.makeSet(100, 10, 10, 10)(strategy)
 
   test("The factory should return a Set of given size"){
     assert(foodSet.size == 100)
-//    assert(creatureSet.size == 100)
+    assert(creatureSet.size == 100)
   }
 
-  test("The coords should all be between boundaries"){
+  test("The coords of every blob should be between boundaries"){
     assert(foodSet.count(f => f.center.x >= 0 && f.center.x <= 100 && f.center.y >= 0 && f.center.y <= 100) == 100)
+    assert(creatureSet.count(f => f.center.x >= 0 && f.center.x <= 100 && f.center.y >= 0 && f.center.y <= 100) == 100)
   }
 
 }
