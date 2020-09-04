2beecbfb44a386a67b32526f53dadae44bcb2ceb
Leonardo <lperugini2@gmail.com>
2020-09-04 12:42:42 +0200

    Include IOFood helper class - now food can be used in for-comprehension

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,10 +1,14 @@
 import model.Position
-import model.entity.IO.IOCreature
+import model.entity.IO.{IOCreature, IOFood}
+
+import scala.util.Random
 
 object Application extends App {
   println("Welcome to natural selection simulator!!!")
 
   for {
     c <- IOCreature.makeIOStarvingCreature(Position(10,10))(10)(10)(10)
+    //f <- IOFood.makeIOFood(Position(10,10))(10)
+    //s <- IOFood.makeIOFoodSet(100)(10)(() => Position(new Random().nextInt(100), new Random().nextInt(100)))
   } yield println(c)
 }
