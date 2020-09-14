4cb2ab070c0fe0b5475c04cdad2b6d2231cb0853
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-14 19:27:13 +0200

    Refined Movement

--- src/main/scala/model/creature/Movement.scala
+++ src/main/scala/model/creature/Movement.scala
@@ -1,5 +1,12 @@
 package model.creature
 
+import model.Position
+
 trait Movement extends Creature { c: Creature =>
-  def move(implicit energyConsumption: (Double, Double) => Double): Creature = ???
+  def goal: Position
+  def move(implicit energyConsumption: (Double, Double) => Double): Creature = this match {
+    case ReproducingCreature(center, speed, energy, radius) => ReproducingCreature(center, speed, energy, radius)
+    case StarvingCreature(center, speed, energy, radius) => StarvingCreature(center, speed, energy, radius)
+    case AteCreature(center, speed, energy, radius) => AteCreature(center, speed, energy, radius)
+  }
 }
