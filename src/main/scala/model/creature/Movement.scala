350188908ded666964c6bc113060ca4aedc9b1e0
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-15 09:49:19 +0200

    Temporary removed move implementation

--- src/main/scala/model/creature/Movement.scala
+++ src/main/scala/model/creature/Movement.scala
@@ -4,9 +4,11 @@ import model.Position
 
 trait Movement extends Creature { c: Creature =>
   def goal: Position
-  def move(implicit energyConsumption: (Double, Double) => Double): Creature = this match {
-    case ReproducingCreature(center, speed, energy, radius) => ReproducingCreature(center, speed, energy, radius)
-    case StarvingCreature(center, speed, energy, radius) => StarvingCreature(center, speed, energy, radius)
-    case AteCreature(center, speed, energy, radius) => AteCreature(center, speed, energy, radius)
-  }
+  def move(implicit energyConsumption: (Double, Double) => Double): Creature = ???
+
+//    c match {
+//    case ReproducingCreature(center, speed, energy, radius) => ReproducingCreature(center, speed, energy, radius)
+//    case StarvingCreature(center, speed, energy, radius) => StarvingCreature(center, speed, energy, radius)
+//    case AteCreature(center, speed, energy, radius) => AteCreature(center, speed, energy, radius)
+//  }
 }
