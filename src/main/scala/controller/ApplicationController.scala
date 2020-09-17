bf62c83df62b5c3d2ec8cea7122703b3f54a2673
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-17 15:14:26 +0200

    Removed comments

--- src/main/scala/controller/ApplicationController.scala
+++ src/main/scala/controller/ApplicationController.scala
@@ -8,34 +8,11 @@ case class ApplicationController() {
 
   var output: Output = Output()
 
-  def execute(simulator: Simulator): Output = {
-
+  def execute(simulator: Simulator): Output =
     if (simulator.hasNext) {
       val nextDay = simulator.next()
       output = log(output)(simulator.executedStep, nextDay.environment)
       execute(nextDay)
     } else output
-  }
+
 }
-
-/*
-    view.update(environment, view.frame) match {
-      case Some(visualizer) => {
-        visualizer.validate()
-        visualizer.repaint()
-        view.frame.get.setSize(SIMULATOR_WIDTH+1, SIMULATOR_HEIGHT+1)
-      }
-      case _ =>
-    }
- */
-
-
-    // display view
-    /*
-    view.frame match {
-      case Some(value) => value.getContentPane.add(Visualizer(environment))
-      case _ => {}
-    }
-     */
-
-
