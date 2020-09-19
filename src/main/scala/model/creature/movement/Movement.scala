8ecb2212b0cf450ec89cec92c261f01b9d5ffaf2
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-19 10:54:21 +0200

    Added comment in Movement.scala

--- src/main/scala/model/creature/movement/Movement.scala
+++ src/main/scala/model/creature/movement/Movement.scala
@@ -6,8 +6,15 @@ import model.{Blob, BlobImplementation}
 import helpers.Strategies._
 import model.creature.movement.EnvironmentCreature.EnvironmentCreature
 
+/** Self type, adding movement to a creature */
 trait Movement extends Creature { c: Creature =>
   def goal: Blob
+
+  /** Makes the creature move
+   *
+   * @param energyConsumption how the energy is decreased
+   * @return a new creature with a new position and (if reached) a new goal
+   */
   def move(implicit energyConsumption: (Double, Double) => Double): EnvironmentCreature = c match {
     case ReproducingCreature(center, speed, energy, radius, _) => ReproducingCreature(randomBoundedPosition, speed, energy - energyConsumption(radius, speed), radius, computeGoal(this))
     case StarvingCreature(center, speed, energy, radius, _) => StarvingCreature(randomBoundedPosition, speed, energy - energyConsumption(radius, speed), radius, computeGoal(this))
