fc5dfa5ebd4cb2282bdd309e2ef04d6f6644dac3
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-19 10:55:01 +0200

    Added comment in Creature.scala

--- src/main/scala/model/creature/Creature.scala
+++ src/main/scala/model/creature/Creature.scala
@@ -4,6 +4,7 @@ import model.{Blob, Position}
 import helpers.Strategies._
 import model.creature.movement.{ReproducingCreature, StarvingCreature}
 
+/** Creature trait */
 trait Creature extends Blob {
   def speed: Double
   def energy: Double
