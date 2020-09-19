e85089e5c907749f87bbf7f35cb7b37b47d8dded
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-19 11:30:37 +0200

    Added comment in DaySimulator.scala

--- src/main/scala/controller/simulator/DaySimulator.scala
+++ src/main/scala/controller/simulator/DaySimulator.scala
@@ -6,6 +6,7 @@ import model.Environment
 import model.creature.movement.EnvironmentCreature
 import view.{SimulationView, View}
 
+/** The day simulator execute an entire day per step */
 case class DaySimulator(executedStep: Int,
                         nFood: Int,
                         nDays: Int,
@@ -16,8 +17,16 @@ case class DaySimulator(executedStep: Int,
   private val sizeMutation = EnvironmentCreature.noSizeMutation
   private val speedMutation = EnvironmentCreature.noSpeedMutation
 
+  /**
+   *
+   * @return true if the simulator can do another step, simulating the following day
+   */
   override def hasNext: Boolean = nDays > 0
 
+  /** Executes a an entire day of the simulation
+   *
+   * @return A new simulator (maybe) ready to simulate another entire day
+   */
   override def next(): Simulator = {
     println("[DAY " + nDays + " ]")
 
