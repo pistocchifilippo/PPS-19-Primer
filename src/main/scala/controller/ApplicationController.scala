49ff5e1d778915a765e3a1bba9ed2625cc49d61f
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-19 11:20:09 +0200

    Added comment in ApplicationController.scala

--- src/main/scala/controller/ApplicationController.scala
+++ src/main/scala/controller/ApplicationController.scala
@@ -8,6 +8,11 @@ case class ApplicationController() {
 
   var output: Output = Output()
 
+  /** This function executes a simulation until it's expired
+   *
+   * @param simulator that will be executed
+   * @return The output of the simulation
+   */
   def execute(simulator: Simulator): Output =
     if (simulator.hasNext) {
       val nextDay = simulator.next()
