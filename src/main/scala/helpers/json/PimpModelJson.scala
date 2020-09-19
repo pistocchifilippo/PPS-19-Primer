5c05ec5b81fee317adb78a5c7e9b79e7670f3be9
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-19 11:17:58 +0200

    Added comment in PimpModelJson.scala

--- src/main/scala/helpers/json/PimpModelJson.scala
+++ src/main/scala/helpers/json/PimpModelJson.scala
@@ -6,10 +6,12 @@ import model.{Environment, Food}
 import model.output.Output.Output
 import play.api.libs.json.{JsObject, Json}
 
+/** In this module there are Pimping for model library, in order to add json conversion */
 object PimpModelJson {
 
   import helpers.json.JsonLabel._
 
+  /** Adds following conversions to Output trait */
   implicit class OutputToJson(output: Output) {
     def outputToJson: JsObject = {
       def toJson(out: Output): JsObject = out.keySet.toList match {
@@ -20,12 +22,14 @@ object PimpModelJson {
     }
   }
 
+  /** Adds following conversions to Environment trait */
   implicit class EnvironmentToJson(environment: Environment) {
     def creaturesToJson: JsObject = Json.obj(CREATURES -> environment.creatures.map(_.creatureToJson))
     def foodsToJson: JsObject = Json.obj(FOOD -> environment.food.map(_.foodToJson))
     def environmentToJson: JsObject = Json.obj(ENVIRONMENT -> (creaturesToJson ++ foodsToJson))
   }
 
+  /** Adds following conversions to Food trait */
   implicit class FoodToJson(f: Food) {
     def xJson: JsObject = Json.obj(X -> f.center.x)
     def yJson: JsObject = Json.obj(Y -> f.center.y)
@@ -33,9 +37,10 @@ object PimpModelJson {
     def foodToJson: JsObject = Json.obj(FOOD -> positionJson)
   }
 
+  /** Adds following conversions to Creature trait */
   implicit class CreatureToJson(c: Creature) {
 
-    def condition: JsObject = {
+    def conditionJson: JsObject = {
       val cond: String = c match {
         case StarvingCreature(_,_,_,_,_) => STARVING
         case AteCreature(_,_,_,_,_) => ATE
@@ -51,7 +56,7 @@ object PimpModelJson {
     def sizeJson: JsObject = Json.obj(SIZE -> c.radius)
     def speedJson: JsObject = Json.obj(SPEED -> c.speed)
 
-    def creatureToJson: JsObject = Json.obj(CREATURE -> (condition ++ sizeJson ++ speedJson ++ positionToJson))
+    def creatureToJson: JsObject = Json.obj(CREATURE -> (conditionJson ++ sizeJson ++ speedJson ++ positionToJson))
   }
 
 }
