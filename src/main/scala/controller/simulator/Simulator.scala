4703f3b0a0177f10a1dc9e98bc58c39bc8aefb3f
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-19 11:25:59 +0200

    Added comment in Simulator.scala

--- src/main/scala/controller/simulator/Simulator.scala
+++ src/main/scala/controller/simulator/Simulator.scala
@@ -2,7 +2,23 @@ package controller.simulator
 
 import model.Environment
 
+/** This trait represent the behaviour of the Simulator
+ *
+ * A simulator extends Iterator behaviour
+ *
+ * A simulator embeds the environment to work with it
+ * A simulator have a number of executed step
+ */
 trait Simulator extends Iterator [Simulator] {
+  /**
+   *
+   * @return the environment of the step
+   */
   def environment: Environment
+
+  /**
+   *
+   * @return the number of executed step
+   */
   def executedStep: Int
 }
