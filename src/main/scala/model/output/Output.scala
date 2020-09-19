e609e33b7219978aac777121736e2352671decb7
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-19 11:13:47 +0200

    Added comment in Output.scala

--- src/main/scala/model/output/Output.scala
+++ src/main/scala/model/output/Output.scala
@@ -3,23 +3,37 @@ package model.output
 import model.Environment
 import play.api.libs.json.Json._
 
+/** Module containing the output */
 object Output {
 
   type Output = Map[Int, Environment]
 
+  /** Creates an empty output
+   *
+   * @return an empty output
+   */
   def apply(): Output = Map.empty
-  def apply(output: Output): Output = output
+
+  /** Makes a new updated output
+   *
+   * @param output The old output
+   * @param day of the simulation
+   * @param environment resulting environment of the simulation
+   * @return The updated output
+   */
   def log(output: Output)(day: Int, environment: Environment): Output = output + (day -> environment)
 
-  // Parsing strategy factories
+  /** Parsing strategy factories */
   private type Parser = Output => String
 
   import helpers.json.PimpModelJson._
 
+  /** Strategy for json string */
   object JsonParser extends Parser {
     override def apply(out: Output): String = out.outputToJson.toString()
   }
 
+  /** Strategy for Cli readable string */
   object CliParser extends Parser {
     override def apply(out: Output): String = prettyPrint(out.outputToJson)
   }
