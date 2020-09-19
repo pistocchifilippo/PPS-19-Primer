4aa9f9b5bdd4b9492aa5a5218e66a926b7e2947b
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-19 11:28:57 +0200

    Added comment in DayStepSimulator.scala

--- src/main/scala/controller/simulator/DayStepSimulator.scala
+++ src/main/scala/controller/simulator/DayStepSimulator.scala
@@ -5,12 +5,21 @@ import model.{Blob, Environment}
 import model.creature.movement.{EnvironmentCreature, ReproducingCreature}
 import view.SimulationView
 
+/** The DayStepSimulator represent the simulation for just one step of just one day */
 case class DayStepSimulator(executedStep: Int, environment: Environment, view: SimulationView) extends Simulator {
 
   implicit val kineticConsumption: (Double, Double) => Double =  EnvironmentCreature.kineticConsumption
 
+  /**
+   *
+   * @return true if the simulator can do another step
+   */
   override def hasNext: Boolean = environment.creatures.count(_.energy > 0) > 0
 
+  /** Executes a step of a day simulation
+   *
+   * @return A new simulator (maybe) ready to simulate another step of the day
+   */
   override def next(): Simulator =  {
 
     val creatures = environment.creatures map (_.move)
