45cd23ae3da1856abb83b25ef9a1e0849e51f584
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-19 10:52:03 +0200

    Added comment in BasicCreature.scala

--- src/main/scala/model/creature/movement/BasicCreature.scala
+++ src/main/scala/model/creature/movement/BasicCreature.scala
@@ -3,6 +3,14 @@ package model.creature.movement
 import model.{Blob, Position}
 import model.creature.movement.EnvironmentCreature.EnvironmentCreature
 
+/** This is a creature that ate no food
+ *
+ * @param center of the creature
+ * @param speed of the creature
+ * @param energy of the creature
+ * @param radius of the creature
+ * @param goal of the creature
+ */
 case class StarvingCreature(
                              override val center: Position,
                              override val speed: Double,
@@ -11,6 +19,14 @@ case class StarvingCreature(
                              override val goal: Blob
                            ) extends EnvironmentCreature
 
+/** This is a creature that ate one food
+ *
+ * @param center of the creature
+ * @param speed of the creature
+ * @param energy of the creature
+ * @param radius of the creature
+ * @param goal of the creature
+ */
 case class AteCreature(
                         override val center: Position,
                         override val speed: Double,
@@ -19,6 +35,14 @@ case class AteCreature(
                         override val goal: Blob
                       ) extends EnvironmentCreature
 
+/** This is a creature that ate two food
+ *
+ * @param center of the creature
+ * @param speed of the creature
+ * @param energy of the creature
+ * @param radius of the creature
+ * @param goal of the creature
+ */
 case class ReproducingCreature(
                                 override val center: Position,
                                 override val speed: Double,
