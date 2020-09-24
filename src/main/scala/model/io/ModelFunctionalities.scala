25ab7dea90d8e3fca51fd310d2721531a735d057
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-09-24 21:33:09 +0200

    Added comments in ModelFunctionalities

--- src/main/scala/model/io/ModelFunctionalities.scala
+++ src/main/scala/model/io/ModelFunctionalities.scala
@@ -1,7 +1,6 @@
 package model.io
 
 import cats.effect.IO
-import controller.simulator.{DaySimulator, Simulator}
 import helpers.Configurations.CREATURES_ENERGY
 import helpers.Strategies.randomGoal
 import model.{Blob, Environment, Food, Position}
@@ -9,13 +8,28 @@ import model.creature.movement.EnvironmentCreature.EnvironmentCreature
 import model.creature.movement.{ReproducingCreature, StarvingCreature}
 import helpers.Configurations._
 import helpers.Strategies._
-import view.SimulationParameters
 
+/** This module contains model functionalities that exploit the IO monad
+ *  This functions can be integrated into the simulator next routine
+ * */
 object ModelFunctionalities {
 
+  /** Moves of one step each creature into the collection
+   *
+   * @param creatures that will be moved
+   * @param energyConsumption that will be decreased to creature energy
+   * @return the new set of moved creatures
+   */
   def moveCreatures(creatures: Traversable[EnvironmentCreature])(implicit energyConsumption: (Double,Double) => Double): IO[Traversable[EnvironmentCreature]] = IO pure {creatures map (_.move)}
 
   type FoodCreatureCollision = (EnvironmentCreature, Food)
+
+  /** Compute the collisions
+   *
+   * @param creatures that have been moved
+   * @param food in the environment
+   * @return A tuple containing creatures and food that collides
+   */
   def collisions(creatures: Traversable[EnvironmentCreature])(food: Traversable[Food]): IO[Traversable[FoodCreatureCollision]] = IO pure {
     for {
       c <- creatures
@@ -33,11 +47,26 @@ object ModelFunctionalities {
   }}
   private def updateFoodCollection(foodCollection: Traversable[Food])(toRemove: List[Food]): IO[Traversable[Food]] = IO pure {foodCollection filter (!toRemove.contains(_))}
 
+  /** Makes a new environment (of the same day) applying changes due to creature movement and collisions
+   *
+   * @param movedCreatures
+   * @param foodCollection
+   * @param coll
+   * @return the new environment
+   */
   def makeNewEnvironment(movedCreatures: Traversable[EnvironmentCreature])(foodCollection: Traversable[Food])(coll: Traversable[FoodCreatureCollision]): IO[Environment] = for {
     c <- updateCreatureCollection(movedCreatures)(collidingCreatures(coll))
     f <- updateFoodCollection(foodCollection)(collidingFood(coll))
   } yield Environment(BOUNDARIES, f, c)
 
+  /** Build the new set of creatures from the creature collection of the day before
+   *
+   * @param creatures
+   * @param pos
+   * @param sizeMutation
+   * @param speedMutation
+   * @return The creature collection of the new day
+   */
   def evolutionSet(creatures: Traversable[EnvironmentCreature])(pos: () => Position)(sizeMutation: Double => Double)(speedMutation: Double => Double): IO[Traversable[EnvironmentCreature]] = IO pure {
     creatures flatMap {
       _ match {
