7690463e3b93d304e459499dc67a2aed3ca0e1d7
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-10-14 12:55:28 +0200

    Modified mock CREATURE as long as movement will be tested correctly

--- src/test/scala/testsUtil/Mock.scala
+++ src/test/scala/testsUtil/Mock.scala
@@ -3,6 +3,7 @@ package testsUtil
 import controller.simulator.DaySimulator
 import helpers.Configurations.BOUNDARIES
 import helpers.Strategies.makeBoundedFoodCollection
+import model.creature.movement.EnvironmentCreature
 import model.creature.movement.EnvironmentCreature.{AteCreature, EnvironmentCreature, ReproducingCreature, StarvingCreature}
 import model.environment.Blob.makeBlobCollection
 import model.environment.Position.Position
@@ -18,7 +19,7 @@ object Mock {
   val MOCK_GOAL: Goal = Goal(30.0 -> 30.0, 10)
   val MOCK_SPEED = 10
   val MOCK_RADIUS = 10
-  val MOCK_ENERGY = 10
+  val MOCK_ENERGY: Double = EnvironmentCreature.kineticConsumption(MOCK_RADIUS,MOCK_SPEED) + 1
   val MOCK_FOOD_SET_SIZE = 100
   val MOCK_CREATURE_SET_SIZE = 100
 
